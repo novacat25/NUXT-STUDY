ddc7d7a1ed6362da2c3b5fb6ef2b3b6f2884cbc1
NovaCat <sparkstorms2@gmail.com>
2024-04-01 19:46:20 +0900

    Feat: add pages

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <h1 class="text-4xl text-blue-600">Hello Nuxt!</h1>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-4xl text-blue-600">Index page</h1>
+    </div>
+</template>
