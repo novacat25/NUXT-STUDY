9a2c12f05abeb29c2643ca7a79de4b3c30a7e927
NovaCat <sparkstorms2@gmail.com>
2024-04-01 19:55:08 +0900

    Feat: use composables

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,3 +1,11 @@
+<script setup>
+const route = useRoute()
+console.log(route.params.id)
+
+const nuxt = useNuxtApp()
+console.log("nuxt",nuxt)
+</script>
+
 <template>
     <div>
         <h2 class="text-4xl text-blue-600">event id: {{ $route.params.id }}</h2>
