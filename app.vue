2cbae358fb3e96ffb42d10fea1552939adcb71f8
NovaCat <sparkstorms2@gmail.com>
2024-04-04 18:45:42 +0900

    Feat: add header with NuxtLink

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <NuxtLayout>
+      <header>
+        <ui>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ui>
+      </header>
       <NuxtPage />
     </NuxtLayout>
   </div>
